24d4fba4793850395462974858f6b8a50354f0e7
zack <twd1000015706@gmail.com>
2022-11-15 19:01:13 +0800

    crud

--- app/controllers/wish_list_controller.rb
+++ app/controllers/wish_list_controller.rb
@@ -1,19 +1,24 @@
 class WishListController < ApplicationController
     def card
+        @wish_lists = WishList.all
     end
 
     def new_wish
+        @wish_list = WishList.new
     end
 
     def card_list
         #準備寫進資料庫
-       clean_params = params.require(:w).permit(:title, :description)
-        w = WishList.new(clean_params)
-        if w.save
-            render html: "ok"
+       clean_params = params.require(:wish_list).permit(:title, :description)
+        @wish_list = WishList.new(clean_params)
+        if @wish_list.save
+            redirect_to make_a_wish_path, notice: "成功新增許願卡"
         else
-            render html: "fails" 
+           render :new_wish
         end
        
+    end
+    def show_wish
+        render html: params
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get "/about" , to:"pages#about"
   get "/contact" , to:"pages#contact"
-  get "/" ,to:"pages#home"
+  get "wish_card_info/:id", to: "wish_list#show_wish" , as: "wish_card_info"
   get "/make_a_wish", to:"wish_list#card"
- get "/new_wish" , to:"wish_list#new_wish"
+  get "/new_wish" , to:"wish_list#new_wish"
   post "/wish_card_list" , to:"wish_list#card_list"
-   root "pages#home"
+  root "wish_list#card"
 end
